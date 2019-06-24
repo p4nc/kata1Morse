c07debea835aa3a56252bb4591508d83a328925a
p4anc <toni7902@hotmail.com>
2019-06-24 22:59:04 +0200

    Version final con mensajes, y envio

--- morse.py
+++ morse.py
@@ -1,49 +1,98 @@
+from docx import Document
+from docx.shared import Pt
+from time import strftime, gmtime
+from datetime import datetime
+datetime.now()
+
 morse = {
-    'A':'·—', 
-    'B':'—···', 
-    'C': '—·—·', 
-    'D': '—··', 
-    'E': '·', 
-    'F': '··—·',  
-    'G': '——·',
-    'H': '····', 
-    'I': '··',
-    'J': '·———',
-    'K': '—·—',
-    'L': '·—··',
-    'M': '——',
-    'N': '—·',
-    'Ñ': '——·——', 
-    'O': '———',
-    'P': '·——·',
-    'Q': '——·—',
-    'R': '·—·', 
-    'S': '···',
-    'T': '—',
-    'U': '··—',
-    'V': '···—',
-    'W': '·——',
-    'X': '—··—',
-    'Y': '—·——', 
-    'Z': '——··',
-    '0': '—————',
-    '1': '·————',
-    '2': '··———',
-    '3': '···——',
-    '4': '····—',
-    '5': '·····',
-    '6': '—····',
-    '7': '——···', 
-    '8':'———··',
-    '9': '————·',
-    '.': '·—·—·—',
-    ',': '—·—·——',
-    '?': '··——··',
-    '"': '·—··—·',
-    '!': '——··——'
+    'A':'.-', 
+    'B':'-...', 
+    'C': '-.-.', 
+    'D': '-..', 
+    'E': '.', 
+    'F': '..-.',  
+    'G': '--.',
+    'H': '....', 
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'Ñ': '--.--', 
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.', 
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--', 
+    'Z': '--..',
+    '0': '-----',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...', 
+    '8':'---..',
+    '9': '----.',
+    '.': '.-.-.-',
+    ',': '-.-.--',
+    '?': '..--..',
+    '"': '.-..-.',
+    '!': '--..--'
 }
 
-
+reverso = {
+   '.-': 'A',
+   '-...': 'B',
+   '-.-.': 'C',
+   '-..': 'D',
+   '.': 'E',
+   '..-.': 'F',
+   '--.': 'G',
+   '....': 'H',
+   '..': 'I',
+   '.---': 'J',
+   '-.-': 'K',
+   '.-..': 'L',
+   '--': 'M',
+   '-.': 'N',
+   '--.--': 'Ñ',
+   '---': 'O',
+   '.--.': 'P',
+   '--.-': 'Q',
+   '.-.': 'R',
+   '...': 'S',
+   '-': 'T',
+   '..-': 'U',
+   '...-': 'V',
+   '.--': 'W',
+   '-..-': 'X',
+   '-.--': 'Y',
+   '--..': 'Z',
+   '-----': '0',
+   '.----': '1',
+   '..---': '2',
+   '...--': '3',
+   '....-': '4',
+   '.....': '5',
+   '-....': '6',
+   '--...': '7',
+   '---..': '8',
+   '----.': '9',
+   '.-.-.-': '.',
+   '-.-.--': ',',
+   '..--..': '?',
+   '.-..-.': '"',
+   '--..--': '!'
+}
 
 def toMorse(texto):
 
@@ -57,5 +106,56 @@ def toMorse(texto):
             resultado += " "
 
     return resultado
+
+def toPlain(codigo):
+    codigo = codigo.split(" ")
+    letras = ""
+    for caracter in codigo:
+        if caracter in reverso:
+            letras += reverso[caracter]
+
+        else:
+            letras += " "
+    return letras.capitalize()
+
+def telegram(remitente,destinatario,mensaje):
+    document = Document()
+
+    document.add_heading('Telegrama', 0)
+
+    fechahora =datetime.now()
+
+    hoy = fechahora.strftime("%d - %b - %Y")
+
+    fecha = document.add_paragraph(hoy)
+    fecha.alignment = 2
+
+    de = document.add_paragraph()
+    de.add_run("From: ").bold = True
+    de.add_run(remitente)
+
+    para = document.add_paragraph()
+    para.add_run("To: ").bold = True
+    para.add_run(destinatario)
+
+    document.add_heading("Mensaje", level=1)
+
+
+    table = document.add_table(rows=2, cols=1)
+    table.style = "LightShading"
+
+    style = table.style
+    font = style.font
+    font.name ="Courier"
+    font.size = Pt(12)
+
+    celda_morse = table.rows[0].cells[0]
+    celda_morse.text= toMorse(mensaje)
+
+    celda_plano = table.rows[1].cells[0]
+    celda_plano.text = mensaje
+
+    document.save("envios/{}{}.docx".format(destinatario, fechahora.strftime("%Y%m%d%H%M%S%f")))
+
 
         
--- morse01.py
+++ morse01.py
@@ -1,3 +1,7 @@
+from docx import Document
+from docx.shared import Pt
+from time import strftime, gmtime
+
 morse = {
     'A':'·—', 
     'B':'—···', 
@@ -42,11 +46,49 @@ morse = {
     '"': '·—··—·',
     '!': '——··——'
 }
-
-reverso = {}
-for key in morse:
-    valor = morse[key]
-    reverso[valor] = key
+reverso = {
+   '·—': 'A',
+   '—···': 'B',
+   '—·—·': 'C',
+   '—··': 'D',
+   '·': 'E',
+   '··—·': 'F',
+   '——·': 'G',
+   '····': 'H',
+   '··': 'I',
+   '·———': 'J',
+   '—·—': 'K',
+   '·—··': 'L',
+   '——': 'M',
+   '—·': 'N',
+   '——·——': 'Ñ',
+   '———': 'O',
+   '·——·': 'P',
+   '——·—': 'Q',
+   '·—·': 'R',
+   '···': 'S',
+   '—': 'T',
+   '··—': 'U',
+   '···—': 'V',
+   '·——': 'W',
+   '—··—': 'X',
+   '—·——': 'Y',
+   '——··': 'Z',
+   '—————': '0',
+   '·————': '1',
+   '··———': '2',
+   '···——': '3',
+   '····—': '4',
+   '·····': '5',
+   '—····': '6',
+   '——···': '7',
+   '———··': '8',
+   '————·': '9',
+   '·—·—·—': '.',
+   '—·—·——': ',',
+   '··——··': '?',
+   '·—··—·': '"',
+   '——··——': '!'}
 
 
 def toMorse(texto):
@@ -73,4 +115,42 @@ def toPlain(codigo):
 
         else:
             letras += " "
-    return letras
+    return letras.capitalize()
+
+def telegram(remitente,destinatario,mensaje):
+    document = Document()
+
+    document.add_heading('Telegrama', 0)
+
+    fechaHora = gmtime()
+    hoy = strftime("%d - %b - %Y",fechaHora)
+
+    fecha = document.add_paragraph(hoy)
+    fecha.alignment = 2
+
+    de = document.add_paragraph()
+    de.add_run("From: ").bold = True
+    de.add_run(Remitente)
+
+    para = document.add_paragraph()
+    para.add_run("To: ").bold = True
+    para.add_run(Destinatario)
+
+    document.add_heading("Mensaje", level=1)
+
+
+    table = document.add_table(rows=2, cols=1)
+    table.style = "LightShading"
+
+    style = table.style
+    font = style.font
+    font.name ="Courier"
+    font.size = Pt(12)
+
+    celda_morse = table.rows[0].cells[0]
+    celda_morse.text= toMorse(mensaje)
+
+    celda_plano = table.rows[1].cells[0]
+    celda_plano.text = mensaje
+
+    document.save("{}{}.docx".format(destinatario, strftime("%Y%m%d%H%M%S%z", fechahora)))  
